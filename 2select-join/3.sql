74421e709307e9b909907961afa53668f846a31b
Arturs <your@email.example>
2024-09-12 10:02:48 +0300

    self join

---
+++ 2select-join/3.sql
@@ -0,0 +1,11 @@
+-- self join
+
+use sql_hr;
+select 
+	e.employee_id,
+    e.first_name,
+    e.last_name,
+    m.reports_to as "manager name"
+from employees e
+join employees m 
+	on e.reports_to = m.employee_id
